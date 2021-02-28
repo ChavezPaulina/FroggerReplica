4494486a1936029980ccdee256363a50353c1f67
ChavezPaulina <77360224+ChavezPaulina@users.noreply.github.com>
2021-02-27 21:51:45 -0600

    Project

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float minSpeed = 8f;
+    public float maxSpeed = 12f;
+    public Rigidbody2D rb;
+     float speed = 1f;
+
+    private void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.fixedDeltaTime * speed); 
+    }
+
+    //Speed of game from slider
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
